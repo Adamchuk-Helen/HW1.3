f71569647610e182140b6cd66595a6ad23d6d554
Adamchuk-Helen <61090204+Adamchuk-Helen@users.noreply.github.com>
2021-07-10 16:17:28 +0300

    Добавьте файлы проекта.

---
+++ WinForm1.3/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WinForm1._3
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyForm));
+            this.button_red = new System.Windows.Forms.Button();
+            this.button_green = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_red
+            // 
+            this.button_red.BackColor = System.Drawing.Color.Red;
+            this.button_red.Location = new System.Drawing.Point(305, 370);
+            this.button_red.Name = "button_red";
+            this.button_red.Size = new System.Drawing.Size(94, 24);
+            this.button_red.TabIndex = 0;
+            this.button_red.Text = "Red button";
+            this.button_red.UseVisualStyleBackColor = false;
+            this.button_red.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // button_green
+            // 
+            this.button_green.BackColor = System.Drawing.Color.Lime;
+            this.button_green.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_green.Location = new System.Drawing.Point(405, 370);
+            this.button_green.Name = "button_green";
+            this.button_green.Size = new System.Drawing.Size(89, 24);
+            this.button_green.TabIndex = 1;
+            this.button_green.Text = "Green button";
+            this.button_green.UseVisualStyleBackColor = false;
+            this.button_green.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Click any button";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_green);
+            this.Controls.Add(this.button_red);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MyForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_red;
+        private System.Windows.Forms.Button button_green;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinForm1.3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm1._3
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static int r = 0;
+        static int g = 0;
+        private void button_red_Click(object sender, EventArgs e)
+        {
+            
+            if (sender == button_red)
+            {
+                r++;
+            }
+            else if (sender == button_green)
+            {
+                g++;
+            }
+
+            label1.Text = "Red button was clicked "+ r + " times. \nGreen button was clicked " + g + " times.";
+        }
+
+
+    }
+
+
+        
+    
+}
